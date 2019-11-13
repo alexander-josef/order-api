da8f431ca6a49435bb2e803306c98ea18901a391
Alexander Josef <alexander.josef@netcetera.com>
2019-11-13 22:20:15 +0100

    adding env variables

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-const https = require("https");
+// no HTTPS configuration when run in google cloud env
+// import * as fs from 'fs'
+// import * as https from 'https'
 const app_1 = require("./app");
-const PORT = process.env.PORT;
-// app.listen(PORT)
-const httpsOptions = {
+const PORT = process.env.PORT || 3000;
+app_1.default.listen(PORT);
+/* const httpsOptions = {
     key: fs.readFileSync('./config/key.pem'),
     cert: fs.readFileSync('./config/cert.pem'),
-};
-https.createServer(httpsOptions, app_1.default).listen(PORT);
+}
+
+https.createServer(httpsOptions,app).listen(PORT)
+
+ */ 
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import * as fs from 'fs'
-import * as https from 'https'
+// no HTTPS configuration when run in google cloud env
+// import * as fs from 'fs'
+// import * as https from 'https'
 import app from './app'
-const PORT = process.env.PORT
-// app.listen(PORT)
+const PORT = process.env.PORT || 3000 
+app.listen(PORT)
 
-const httpsOptions = {
+/* const httpsOptions = {
     key: fs.readFileSync('./config/key.pem'),
     cert: fs.readFileSync('./config/cert.pem'),
 }
 
 https.createServer(httpsOptions,app).listen(PORT)
+
+ */
